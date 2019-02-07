6b9a52738e0d299ad49794cc5db5aefc959ad14a
star4z <bdphillips013@gmail.com>
2019-02-07 15:49:14 -0500

    Initial commit Created main class and some layouts

---
+++ app/src/main/java/edu/sunypoly/inventoryapp/ListAllItemsActivity.java
@@ -0,0 +1,15 @@
+package edu.sunypoly.inventoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ListAllItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_all_items);
+    }
+
+}
---
+++ app/src/main/java/edu/sunypoly/inventoryapp/MainActivity.java
@@ -0,0 +1,31 @@
+package edu.sunypoly.inventoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    void login(View vew){
+
+    }
+
+    void listAll(View view){
+
+    }
+
+    void search(View view){
+
+    }
+
+    void addItem(View view){
+
+    }
+}
