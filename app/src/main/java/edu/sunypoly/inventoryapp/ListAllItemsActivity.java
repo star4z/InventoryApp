2aae97e4a1dac46ab4662424b886e7ca47747307
star4z <bdphillips013@gmail.com>
2019-02-26 15:03:08 -0500

    Fixed bug where ListAllItemsActivity would only show 1 item.

--- app/src/main/java/edu/sunypoly/inventoryapp/ListAllItemsActivity.java
+++ app/src/main/java/edu/sunypoly/inventoryapp/ListAllItemsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,6 +12,8 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class ListAllItemsActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
 
     private ArrayList<InventoryItem> inventoryItems;
     private RecyclerView recyclerView;
@@ -29,17 +32,22 @@ public class ListAllItemsActivity extends AppCompatActivity {
 
         inventoryItems = getItems();
 
+        Log.v(TAG, inventoryItems.toString());
+
         if (inventoryItems != null) {
 
             recyclerView = findViewById(R.id.recycler_view);
 
-            recyclerView.setHasFixedSize(true);
 
             layoutManager = new LinearLayoutManager(this);
             recyclerView.setLayoutManager(layoutManager);
 
             mAdapter = new ItemRecyclerAdapter(inventoryItems);
             recyclerView.setAdapter(mAdapter);
+
+//            recyclerView.requestLayout();
+//            recyclerView.setHasFixedSize(true);
+
         } else {
             Toast.makeText(this, "No items to display.", Toast.LENGTH_LONG).show();
         }
